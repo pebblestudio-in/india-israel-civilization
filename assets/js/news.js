5722b5c1dc5dc7b899ad557dd7a25b2efd6237bf
news gatherer <actions@github.com>
2026-08-28 01:37:48 +0000

    Headlines gathered automatically

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,2 +1,34 @@
 /* Written automatically by .github/workflows/news.yml. Do not edit by hand. */
-window.NEWS_DATA = { generated: null, items: [], feedsTried: [], feedsOk: [], feedsFailed: [] };
+window.NEWS_DATA = {
+  "generated": "2026-08-28 01:37 UTC",
+  "items": [],
+  "feedsTried": [
+    "Press Information Bureau",
+    "The Hindu, International",
+    "Indian Express, World",
+    "Times of India, World",
+    "The Times of Israel",
+    "The Jerusalem Post",
+    "BBC World"
+  ],
+  "feedsOk": [
+    "The Hindu, International",
+    "Times of India, World",
+    "The Jerusalem Post",
+    "BBC World"
+  ],
+  "feedsFailed": [
+    {
+      "name": "Press Information Bureau",
+      "reason": "HTTP 403"
+    },
+    {
+      "name": "Indian Express, World",
+      "reason": "HTTP 403"
+    },
+    {
+      "name": "The Times of Israel",
+      "reason": "HTTP 403"
+    }
+  ]
+};
